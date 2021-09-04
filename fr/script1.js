8b249106e85d7bff2f40483693212a245670f54a
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-09-04 20:40:58 +0000

    adding more info to the page.

---
+++ fr/script1.js
@@ -0,0 +1,2 @@
+const miseajour = moment().format('MMMM Do YYYY, h:mm:ss a');
+document.getElementById("miseajour").innerHTML = miseajour;
---
+++ script.js
@@ -0,0 +1,2 @@
+const updatetime = moment().format('MMMM Do YYYY, h:mm:ss a');
+document.getElementById("updatetime").innerHTML = updatetime;
